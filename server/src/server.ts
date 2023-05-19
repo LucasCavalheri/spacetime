fc4ae66d3438a9c689551cc3ad87fdfc07d3e269
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-19 20:09:43 -0300

    configurações iniciais para o BackEnd

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client'
+import fastify from 'fastify'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
