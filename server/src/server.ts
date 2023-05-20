1d811ed4de415bed8326b4ab2a40730ef884b549
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-19 23:34:07 -0300

    feat(server): adiciona o CRUD de memórias

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,9 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-  return users
-})
+app.register(memoriesRoutes)
 
 app
   .listen({
