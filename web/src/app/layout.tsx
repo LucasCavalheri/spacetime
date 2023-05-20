f74d1479fe13105746cec1abd22afdd3e66b4267
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-20 18:44:21 -0300

    feat(web): listagem das memórias

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <Copyright />
           </div>
           {/* Right */}
-          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
             {children}
           </div>
         </main>
--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function NewMemory() {
   return (
-    <div className="flex flex-1 flex-col gap-4">
+    <div className="flex flex-1 flex-col gap-4 p-16">
       <Link
         href="/"
         className="flex items-center gap-1 text-sm text-gray-200 hover:text-gray-100"
