0b32ae24c0d3e2e4e938e16df10f7a72af58efac
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-20 00:53:59 -0300

    feat(server): adiciona rota de autenticação e JWT

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,9 @@
+import 'dotenv/config'
+
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import fastify from 'fastify'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
@@ -7,7 +11,13 @@ const app = fastify()
 app.register(cors, {
   origin: true,
 })
+
+app.register(jwt, {
+  secret: process.env.JWT_SECRET as string,
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
