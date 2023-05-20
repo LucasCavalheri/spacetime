cccfcbed2e8d89defbaa2c56fddb8b4a074484e6
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-20 12:55:23 -0300

    feat(web): adiciona rota de logout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,12 @@
+import { Copyright } from '@/components/Copyright'
+import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
+import { SignIn } from '@/components/Signin'
 import {
   Bai_Jamjuree as BaiJamjuree,
   Roboto_Flex as Roboto,
 } from 'next/font/google'
+import { cookies } from 'next/headers'
 import { ReactNode } from 'react'
 import './globals.css'
 
@@ -19,12 +24,29 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="pt-br">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+            {/* Stripes */}
+            <div className="absolute bottom-0 right-0 top-0 w-2  bg-stripes" />
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
+          {/* Right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
---
+++ web/src/app/memories/new/page.tsx
@@ -0,0 +1,7 @@
+export default function NewMemory() {
+  return (
+    <div>
+      <h1>New</h1>
+    </div>
+  )
+}
