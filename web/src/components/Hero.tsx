f051747f7d1918127dd9981b45fe297f39a294c3
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-20 13:26:06 -0300

    feat(web): adiciona middleware

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import Link from 'next/link'
 
 export function Hero() {
   return (
@@ -14,12 +15,12 @@ export function Hero() {
           com o mundo!
         </p>
       </div>
-      <a
-        href="/"
+      <Link
+        href="/memories/new"
         className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
       >
         CADASTRAR LEMBRANÇA
-      </a>
+      </Link>
     </div>
   )
 }
