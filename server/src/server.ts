6292b29fc0febd7bc594af1fd9b7b98d62441d31
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-19 23:35:58 -0300

    feat(server): adiciona CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
+import cors from '@fastify/cors'
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
+app.register(cors, {
+  origin: true,
+})
 app.register(memoriesRoutes)
 
 app
