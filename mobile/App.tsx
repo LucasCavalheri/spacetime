afecdf7ac999577201e3ad86bfb4a0818c5542bb
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-19 22:38:31 -0300

    feat(mobile): adiciona configuração das fontes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) return null
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-5xl font-bold text-gray-50">Hello World!</Text>
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="font-body text-5xl font-bold text-gray-50">
+        Hello World!
+      </Text>
       <StatusBar style="light" translucent />
     </View>
   )
