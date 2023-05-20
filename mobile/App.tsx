3804c6a2766300687dc8cc28712e55875495a8ad
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-19 23:02:55 -0300

    feat(mobile): finaliza a HOME

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import blurBg from './src/assets/bg-blur.png'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import Stripes from './src/assets/stripes.svg'
 
 import {
   Roboto_400Regular,
@@ -8,6 +11,9 @@ import {
 } from '@expo-google-fonts/roboto'
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -19,11 +25,36 @@ export default function App() {
   if (!hasLoadedFonts) return null
 
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="font-body text-5xl font-bold text-gray-50">
-        Hello World!
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e comparilhe (se quiser)
+            com o mundo!
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar Lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito por Lucas Cavalheri
       </Text>
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
