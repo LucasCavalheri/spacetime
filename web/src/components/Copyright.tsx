583990eaf308d75bd84d7716adf14e60c7ac20be
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-20 00:32:04 -0300

    feat(web): componentiza a Home

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito por{' '}
+      <a
+        href="https://linkedin.com/in/lucas-cavalheri"
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+      >
+        Lucas Cavalheri
+      </a>
+    </div>
+  )
+}
---
+++ web/src/components/Hero.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+
+export function Hero() {
+  return (
+    <div className="space-y-5">
+      <Image src={nlwLogo} alt="NLW Spacetime" />
+      <div className="max-w-[420px] space-y-1">
+        <h1 className="text-5xl font-bold leading-tight text-gray-50">
+          Sua cápsula do tempo
+        </h1>
+        <p className="text-lg leading-relaxed">
+          Colecione momentos marcantes da sua jornada e compartilhe (se quiser)
+          com o mundo!
+        </p>
+      </div>
+      <a
+        href="/"
+        className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+      >
+        CADASTRAR LEMBRANÇA
+      </a>
+    </div>
+  )
+}
