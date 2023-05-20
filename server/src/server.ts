fd31ca60906b446d0146a6bb3eea14e1dfd46825
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-20 14:37:22 -0300

    feat(server): rota de upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,22 @@ import 'dotenv/config'
 
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import fastify from 'fastify'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+const port = Number(process.env.PORT) || 3333
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -16,12 +27,13 @@ app.register(jwt, {
   secret: process.env.JWT_SECRET as string,
 })
 
-app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
-    port: 3333,
+    port,
     host: '0.0.0.0',
   })
   .then(() => {
