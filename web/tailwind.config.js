ced7152aa8e043ce13b0dca07eb4134e84730172
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-19 22:24:06 -0300

    feat(web): finaliza pagina Home

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -53,6 +53,9 @@ module.exports = {
         stripes:
           'linear-gradient(to bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1) 12.5%, transparent 12.5%, transparent)',
       },
+      fontSize: {
+        '5xl': '2.5rem',
+      },
       backgroundSize: {
         stripes: '100% 8px',
       },
