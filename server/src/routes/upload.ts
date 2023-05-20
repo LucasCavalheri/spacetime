832eefc4bf04652e09b023c4dccd2205274ca009
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-20 18:14:12 -0300

    fix: pequenas correções

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -1,7 +1,7 @@
-import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'node:crypto'
-import { createWriteStream } from 'node:fs'
 import { extname, resolve } from 'node:path'
+import { FastifyInstance } from 'fastify'
+import { createWriteStream } from 'node:fs'
 import { pipeline } from 'node:stream'
 import { promisify } from 'node:util'
 
@@ -11,7 +11,7 @@ export async function uploadRoutes(app: FastifyInstance) {
   app.post('/upload', async (request, reply) => {
     const upload = await request.file({
       limits: {
-        fileSize: 5_242_800, // 5MB
+        fileSize: 5_242_880, // 5mb
       },
     })
 
@@ -22,7 +22,9 @@ export async function uploadRoutes(app: FastifyInstance) {
     const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
     const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
 
-    if (!isValidFileFormat) return reply.status(400).send()
+    if (!isValidFileFormat) {
+      return reply.status(400).send()
+    }
 
     const fileId = randomUUID()
     const extension = extname(upload.filename)
@@ -30,13 +32,13 @@ export async function uploadRoutes(app: FastifyInstance) {
     const fileName = fileId.concat(extension)
 
     const writeStream = createWriteStream(
-      resolve(__dirname, '../../uploads/', fileName),
+      resolve(__dirname, '..', '..', 'uploads', fileName),
     )
 
     await pump(upload.file, writeStream)
 
     const fullUrl = request.protocol.concat('://').concat(request.hostname)
-    const fileUrl = new URL(`/upload/${fileName}`, fullUrl).toString()
+    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
 
     return { fileUrl }
   })
