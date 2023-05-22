4c6842391ee46663bd3c0ea2471432651420f6ab
LucasCavalheri <cav.catecumenal@gmail.com>
2023-05-21 22:48:09 -0300

    feat(server): deleção de uma memória

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -70,11 +70,14 @@ export async function memoriesRoutes(app: FastifyInstance) {
     const paramsSchema = z.object({
       id: z.string().uuid(),
     })
+
     const bodySchema = z.object({
       content: z.string(),
       coverUrl: z.string(),
       isPublic: z.coerce.boolean().default(false),
     })
+
+    console.log(request.body)
 
     const { id } = paramsSchema.parse(request.params)
     const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
